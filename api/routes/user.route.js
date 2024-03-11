5d773ee7b83bcc75714e12a11c78cda7a17cf25c
Osman Arda Akdoğan <osmanardakdgn@gmail.com>
2024-03-11 20:30:31 +0100

    user update functions

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import userCntrl from "../controllers/user.controller.js"
+import { updateUser } from "../controllers/user.controller.js"
+import {verifyToken} from "../utils/verifyUser.js";
 
 const router = express.Router();
 
-router.get("/test", userCntrl);
+router.post("/update/:id", verifyToken, updateUser);
 
 export default router;
