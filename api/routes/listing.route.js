a088243568338c8fb2a225ed45d3230c0ed5cab1
Osman Arda Akdoğan <osmanardakdgn@gmail.com>
2024-03-15 00:37:48 +0100

    searching api functions

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { verifyToken } from "../utils/verifyUser.js";
-import { createListing, deleteListing, getListing, updateListing } from "../controllers/listing.controller.js";
+import { createListing, deleteListing, getListing, getListings, updateListing } from "../controllers/listing.controller.js";
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.post('/update/:id', verifyToken, updateListing );
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.get('/get', getListings);
 router.get('/get/:id', getListing);
 
 
